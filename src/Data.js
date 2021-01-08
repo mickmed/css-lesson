400a99814093b46fab467b64c217e4255e621233
mickmed <mick.roth@generalassemb.ly>
2021-01-07 20:23:18 -0500

    init

---
+++ src/Data.js
@@ -0,0 +1,113 @@
+const products = [
+    {
+      "name": "Ravi Shankar",
+      "price": 2.99,
+      "description": "Sitar Times",
+      "pic": "ravishankar.jpg"
+    },
+    {
+      "name": "Samuel Barber",
+      "price": 5.99,
+      "description": "Adagio for Strings",
+      "pic": "adagioforstrings.jpg"
+    },
+    {
+      "name": "Suzi Quatro",
+      "price": 0.99,
+      "description": "Can the Can",
+      "pic": "canthecan.jpg"
+    },
+    {
+      "name": "Devo",
+      "price": 10.59,
+      "description": "Duty Now For the Future",
+      "pic": "dutynowforthefuture.jpg"
+    },
+    {
+      "name": "Fanny",
+      "price": 3.99,
+      "description": "Charity Ball",
+      "pic":"charityball.jpg"
+    },
+    {
+      "name": "Gillian Welch",
+      "price": 5.99,
+      "description": "Gillian Welch",
+      "pic":"gillianwelch.jpg"
+    },
+    {
+      "name": "Junior Parker",
+      "price": 3.99,
+      "description": "Love Ain't Nothin' but a Business Going on",
+      "pic":"juniorparker.jpg"
+    },
+    {
+      "name": "Donna Summer",
+      "price": 6.55,
+      "description": "Love to Love You",
+      "pic":"lovetoloveyou.jpg"
+    },
+    {
+      "name": "motorhead",
+      "price": 3.99,
+      "description": "No Sleep 'til Harrmersmith.",
+      "pic": "motorhead.jpg"
+    },
+    {
+      "name": "stones",
+      "price": 5.99,
+      "description": "Her Majesty's",
+      "pic": "stones.jpg"
+    },
+    // {
+    //   "name": "hair brush",
+    //   "price": 6.99,
+    //   "description": "fine boar bristles"
+    // },
+    // {
+    //   "name": "iridium (one gram)",
+    //   "price": 19.36,
+    //   "description": "corrosion-resistant metal"
+    // },
+    // {
+    //   "name": "quark",
+    //   "price": 0.01,
+    //   "description": "Very small"
+    // },
+    // {
+    //   "name": "turtleneck",
+    //   "price": 19.99,
+    //   "description": "available in black and slightly-darker black"
+    // },
+    // {
+    //   "name": "kaleidoscope",
+    //   "price": 8.25,
+    //   "description": "tube with moving plastic pieces inside"
+    // },
+    // {
+    //   "name": "mitt (leather)",
+    //   "price": 15,
+    //   "description": "regulation sized"
+    // },
+    // {
+    //   "name": "nothing",
+    //   "price": 10,
+    //   "description": "Hey, if you pay us, we won't ask any questions."
+    // },
+    // {
+    //   "name": "violin",
+    //   "price": 2000,
+    //   "description": "Talk about a JS fiddle..."
+    // },
+    // {
+    //   "name": "yoyo",
+    //   "price": 1,
+    //   "description": "We had to pull some strings to get this one in."
+    // },
+    // {
+    //   "name": "pincushion",
+    //   "price": 1.99,
+    //   "description": "You'll get 'stuck' on it"
+    // },
+  ];
+export default products;
